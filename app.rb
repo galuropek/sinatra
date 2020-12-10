bc3d1f621e32ee2cf8fd38c79b07ee951bfd8749
galuropek <galuropek@gmail.com>
2020-12-10 19:56:11 +0300

    practice

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require 'sinatra/base'
+require 'pry'
 
 class MyApp < Sinatra::Base
   set :root, File.dirname(__FILE__)
+  set :sessions, true
 
   get '/' do
     erb :index
+  end
+
+  post '/sign_in' do
+    session[:current_user] = params[:login]
+    binding.pry
+    redirect to('/user')
+  end
+
+  get '/user' do
+    @user = session[:current_user]
+    binding.pry
+    erb :user
   end
 end
